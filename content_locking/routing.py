c163e979ba734b984bdc881cbc271e78a72d75d2
Michael Hearn <git@mikehearn.net>
2025-05-27 14:44:26 -0400

    Refactor websocket URL pattern to use as_asgi() for PresenceConsumer

--- content_locking/routing.py
+++ content_locking/routing.py
@@ -3,5 +3,8 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path('^admin/ws/content_editing/(?P<room_name>.*)/', consumers.PresenceConsumer)
+    re_path(
+        r'^admin/ws/content_editing/(?P<room_name>.*)/',
+        consumers.PresenceConsumer.as_asgi(),
+    )
 ]
